da5498400ef16b204d531e6a77a7858a21b67412
Adrian Teske <adrian.teske808@gmail.com>
2022-01-27 10:50:18 +0100

    Passwort von mindestens 8 Zeichen

--- Passwort.java
+++ Passwort.java
@@ -5,10 +5,7 @@ import static lib.Input.inputString;
 public class Passwort {
     public static void main(String[] args) {
 
-
         pwchecker();
-
-
 
     }
 
@@ -16,6 +13,11 @@ public class Passwort {
         for(int i = 0; i < 3; i++){
             String pw = inputString("Bitte geben Sie ein Passwort ein.");
         String pw2 = inputString("Bitte wiederholen Sie Ihr Passwort ein.");
+
+        if(pw.length() < 8 && pw2.length() < 8){
+                System.out.println("Ihr Passwort ist zu kurz");
+            continue;
+        }
             if (pw.equals(pw2)) {
                 System.out.println("cool");
                 break;
